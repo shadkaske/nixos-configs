eedc6a4bf774cc1562d6a97ce1250d8ea97c4d69
Shad Kaske <shadkaske@gmail.com>
2024-12-08 12:37:44 -0600

    Fixing broken stuff for fsh

--- home-manager/shadkaske/features/shell/fast-syntax-highlighting.nix
+++ home-manager/shadkaske/features/shell/fast-syntax-highlighting.nix
@@ -1,8 +1,8 @@
 { ... }: {
     home.file = {
-        "catppuccin-mocha.ini" = {
+        ".config/fsh/catppuccin-mocha.ini" = {
             enable = true;
-            source = builtins.readFile(builtins.fetchurl {
+            text = builtins.readFile(builtins.fetchurl {
                 url = "https://raw.githubusercontent.com/catppuccin/zsh-fsh/refs/heads/main/themes/catppuccin-mocha.ini";
                 sha256 = "sha256:0kk1615ksfql0qmsibngd4p4mvl1g7mx4hdhji8qd51ip22rds32";
             });
