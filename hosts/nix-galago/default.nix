9365f7c0b4bfed549854b1e067bdf595184bacbb
Shad Kaske <shadkaske@gmail.com>
2024-12-08 10:11:14 -0600

    Host config updates

--- hosts/nix-galago/default.nix
+++ hosts/nix-galago/default.nix
@@ -78,27 +78,6 @@ nix = let
 
   networking.hostName = "nix-galago";
 
-  # TODO: Remove networking moved to module
-  networking.networkmanager.enable = true;
-
-  # TODO: Moved to moduel, delete locale settings Set your time zone.
-  # time.timeZone = "America/Chicago";
-
-  # Select internationalisation properties.
-  # i18n.defaultLocale = "en_US.UTF-8";
-
-  # i18n.extraLocaleSettings = {
-  #   LC_ADDRESS = "en_US.UTF-8";
-  #   LC_IDENTIFICATION = "en_US.UTF-8";
-  #   LC_MEASUREMENT = "en_US.UTF-8";
-  #   LC_MONETARY = "en_US.UTF-8";
-  #   LC_NAME = "en_US.UTF-8";
-  #   LC_NUMERIC = "en_US.UTF-8";
-  #   LC_PAPER = "en_US.UTF-8";
-  #   LC_TELEPHONE = "en_US.UTF-8";
-  #   LC_TIME = "en_US.UTF-8";
-  # };
-
   # Enable the X11 windowing system.
   # You can disable this if you're only using the Wayland session.
   services.xserver.enable = true;
--- hosts/nix-quickemu/default.nix
+++ hosts/nix-quickemu/default.nix
@@ -3,10 +3,11 @@
 # and in the NixOS manual (accessible by running ‘nixos-help’).
 
 {
+  inputs,
+  outputs,
   lib,
   config,
   pkgs,
-  inputs,
   ...
 }: {
   imports =
@@ -15,7 +16,36 @@
       ../../modules/nixos/gnome.nix
     ];
 
-  nixpkgs.config.allowUnfree = lib.mkForce true;
+  nixpkgs = {
+    overlays = [
+      outputs.overlays.additions
+      outputs.overlays.modifications
+      outputs.overlays.unstable-packages
+    ];
+
+    config = {
+      allowUnfree = lib.mkForce true;
+    };
+  };
+
+nix = let
+    flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+  in {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Opinionated: disable global registry
+      flake-registry = "";
+      # Workaround for https://github.com/NixOS/nix/issues/9574
+      nix-path = config.nix.nixPath;
+    };
+    # Opinionated: disable channels
+    channel.enable = false;
+
+    # Opinionated: make flake registry and nix path match flake inputs
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
