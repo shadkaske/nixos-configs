46ffc6c29942a79f71cc53beb73af48a27f6fc1d
Shad Kaske <shadkaske@gmail.com>
2024-12-08 08:21:57 -0600

    Default module for home manager user

--- home-manager/shadkaske/default.nix
+++ home-manager/shadkaske/default.nix
@@ -10,14 +10,7 @@
 }: {
   # You can import other home-manager modules here
   imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
+    ./features/shell
   ];
 
   nixpkgs = {
@@ -45,10 +38,9 @@
     };
   };
 
-  # TODO: Set your username
   home = {
-    username = "your-username";
-    homeDirectory = "/home/your-username";
+    username = "shadkaske";
+    homeDirectory = "/home/shadkaske";
   };
 
   # Add stuff for your user as you see fit:
@@ -63,5 +55,5 @@
   systemd.user.startServices = "sd-switch";
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "23.05";
+  home.stateVersion = "24.11";
 }
