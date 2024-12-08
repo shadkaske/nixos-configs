e3c65e64d38d3c282ca5cfc950111c27f89193e4
Shad Kaske <shadkaske@gmail.com>
2024-12-08 13:50:43 -0600

    Attempting to install a newer version of fzf

--- home-manager/shadkaske/features/shell/fzf.nix
+++ home-manager/shadkaske/features/shell/fzf.nix
@@ -1,9 +1,10 @@
-{
+{ nixpkgs-unstable, ... }: {
   # TODO: Configure FZF
   programs.fzf = {
     enable = true;
     enableBashIntegration = true;
     enableZshIntegration = true;
+    package = nixpkgs-unstable.fzf;
     tmux = {
       enableShellIntegration = true;
       # TODO: Set up fzf Tmux Config
