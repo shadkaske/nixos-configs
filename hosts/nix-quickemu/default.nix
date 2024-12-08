1e79d0db594780ad4f53ff9e953d81ed4b2ed615
Shad Kaske <shadkaske@gmail.com>
2024-12-08 08:53:48 -0600

    Removed duplicate environment variable

--- hosts/nix-quickemu/default.nix
+++ hosts/nix-quickemu/default.nix
@@ -109,8 +109,6 @@
     wget
   ];
 
-  environment.variables.EDITOR = "nvim";
-
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
